9bf37c34f5d02d240edec834f9da34574de7e1e3
101002060 <huangshuaishuai@huaqin.com>
2017-05-03 09:45:53 +0800

    [HSSLAZYWORK][][]init hsslazywork

---
+++ HssLazyWork/app/src/main/java/com/googlog/www/hsslazywork/MainActivity.java
@@ -0,0 +1,17 @@
+package com.googlog.www.hsslazywork;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.googlog.www.hsslazylibrary.util.DateUtil;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int day = DateUtil.getDay(new Date());
+    }
+}
